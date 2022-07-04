2d76291c035a78b0b003d8ed6fd7f88a76080fca
Cemilikon <vovachernyy7@gmail.com>
2022-07-04 21:37:02 +0700

    adding websocket server

---
+++ server_plus_websocket/app.js
@@ -0,0 +1,4 @@
+const status = document.getElementById('status');
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
